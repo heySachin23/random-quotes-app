152689111f33253f37f937ed997a8ad2c2bc7a7c
Sachin Bhadoria <mbsachin7@gmail.com>
2021-06-25 18:31:14 +0530

    Added maxLength constraint for quotes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const quoteVal = document.getElementById("quote");
 const authorVal = document.getElementById("author");
 
 const fetchQuote = () => {
-  fetch("https://api.quotable.io/random")
+  fetch("https://api.quotable.io/random?minLength=100&maxLength=140")
     .then((response) => response.json())
     .then((data) => {
       console.log(`${data.content} —${data.author}`);
